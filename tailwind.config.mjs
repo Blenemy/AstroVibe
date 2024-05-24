3a3ad7eea3cc53c87bdf09390f562111a1b98f18
Denys Topchyi <miliarder97@gmail.com>
2024-05-24 14:58:23 +0300

    feat: add tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,50 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        "cw-1440": "1440px",
+      },
+      colors: {
+        "text-grey": "#cecfd7",
+        "insights-bg": "#030631",
+        "benefits-bg": "#1a1d44",
+        "tos-text-color": "#8d8e9f",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+        "semi-bold": 600,
+      },
+      screens: {
+        "cs-1440": "1440px",
+        "cs-1150": "1150px",
+      },
+      fontSize: {
+        12: "12px",
+        14: "14px",
+        16: "16px",
+        22: "22px",
+      },
+      lineHeight: {
+        16: "16px",
+        20: "20px",
+        24: "24px",
+        28: "28px",
+      },
+      letterSpacing: {
+        0.1: "0.1px",
+        0.25: "0.25px",
+        0.4: "0.4px",
+        0.5: "0.5px",
+        1: "1px",
+      },
+    },
+  },
+  plugins: [],
+};
